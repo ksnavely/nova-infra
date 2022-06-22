6056385e26a89af0bd6d58519785764cc4b614bf
ksnavely <kjsnavely@gmail.com>
2022-06-21 20:53:29 -0400

    Add kdevops-website helm chart (pods don't run)

--- kubernetes/charts/install.sh
+++ kubernetes/charts/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+# Trino, SQL at scale
 helm repo add trino https://trinodb.github.io/charts/
 helm install nt-trino trino/trino --version 0.8.0
+
+# kdevops.com
+helm install kdevops-website kdevops-website/ --values kdevops-website/values.yaml
 
 
--- terraform/kube/security-groups.tf
+++ terraform/kube/security-groups.tf
@@ -4,12 +4,14 @@ resource "aws_security_group" "worker_group_mgmt_one" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
+    from_port = 80
+    to_port   = 80
     protocol  = "tcp"
 
     cidr_blocks = [
       "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
     ]
   }
 }
