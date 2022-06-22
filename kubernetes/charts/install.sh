a5f3952105be1ead3c18dd86ac83cdcdf974bbed
ksnavely <kjsnavely@gmail.com>
2022-06-21 22:09:27 -0400

    Add letsencrypt + cert-manager, cervyd.com for testing

--- kubernetes/charts/install.sh
+++ kubernetes/charts/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# x509 Certificate management
+helm repo add jetstack https://charts.jetstack.io
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.5.3 --set installCRDs=true
+
 # Trino, SQL at scale
 helm repo add trino https://trinodb.github.io/charts/
 helm install nt-trino trino/trino --version 0.8.0
