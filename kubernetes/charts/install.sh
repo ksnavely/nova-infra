e177120ba7da83925d4a1bca417f0daaf60285f1
ksnavely <kjsnavely@gmail.com>
2022-06-23 23:29:02 -0400

    Deploy MongoDB (blogs uploaded to blogs DB). Also enable website TLS with letsencrypt production.

--- kubernetes/charts/install.sh
+++ kubernetes/charts/install.sh
@@ -8,7 +8,13 @@ helm install cert-manager cert-manager/ --namespace cert-manager --create-namesp
 # Trino, SQL at scale
 helm install nt-trino trino/
 
+if [ -z "$MONGODB_ROOT_PASSWORD" -o -z "$MONGO_USR" -o -z "$MONGO_PW" ]; then
+  echo "Cowardly refusing to configure MongoDB without auth config."
+else
+  helm install mongo001 mongodb/ --set "auth.rootPassword=$MONGO_ROOT_PASSWORD,auth.username=$MONGO_USR,auth.password=$MONGO_PW,auth.database=ktest"
+fi
+
 # kdevops.com
-helm install kdevops-website kdevops-website/ --values kdevops-website/values.yaml
+helm install kdevops-website kdevops-website/ --values kdevops-website/values.yaml --set website_json_conf="`cat kdevops-website/website.json | base64`"
 
 
