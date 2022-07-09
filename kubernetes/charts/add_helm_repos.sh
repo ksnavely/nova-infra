de7dee21ead4818b24511474d6b156e6b84738ac
ksnavely <kjsnavely@gmail.com>
2022-07-09 12:31:15 -0400

    TiDB is up

--- kubernetes/charts/add_helm_repos.sh
+++ kubernetes/charts/add_helm_repos.sh
@@ -4,4 +4,6 @@ helm repo add jetstack https://charts.jetstack.io # cert-manager
 helm repo add trino https://trinodb.github.io/charts/
 helm repo add bitnami https://charts.bitnami.com/bitnami # mongodb, grafana but many charts available
 helm repo add datadog https://helm.datadoghq.com
+helm repo add pingcap https://charts.pingcap.org/
+
 helm repo update
